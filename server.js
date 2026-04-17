b7b5ab23487a54f29e090709c47d45a95580c19d
Giridharan <giridharan.k@sync-dynamics.com>
2026-04-18 01:19:49 +0530

    update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 4000;
 
 app.use(cors({
   origin: process.env.ALLOWED_ORIGIN || 'http://localhost:5173',
-  methods: ['GET', 'POST', 'OPTIONS'],
+  methods: ['POST'],
 }));
 app.use(express.json());
 
